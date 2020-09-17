daa29f7428912feb050137c76c411d6ad3ea719f
zimahaba <pmaquitorres@gmail.com>
2020-09-17 08:40:25 -0300

    first commit

---
+++ feign-service/src/test/java/edu/zimahaba/feignservice/FeignServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zimahaba.feignservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
